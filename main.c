e97b8e1fb57d051ff4bf6889649fde15eb62066c
probhav <57523223+probhav@users.noreply.github.com>
2020-10-01 03:29:57 +0530

    Solution #8(Largest element in array)

--- main.c
+++ main.c
@@ -91,7 +91,25 @@ void main()
     break;
   }
   case 8:
-  {
+  { 
+    int i, n;
+   
+    printf("Enter the number of elements (1 to 100): ");
+    scanf("%d", &n);
+    int arr[100];
+    for (i = 0; i < n; ++i) {
+        printf("Enter number%d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // storing the largest number to arr[0]
+    for (i = 1; i < n; ++i) {
+        if (arr[0] < arr[i])
+            arr[0] = arr[i];
+    }
+
+    printf("Largest element =", arr[0]);
+
     break;
   }
   case 9:
