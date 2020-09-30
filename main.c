e54ca5c907f33f1d8f2dc804960fab05bd5a7e3d
probhav <57523223+probhav@users.noreply.github.com>
2020-10-01 03:25:14 +0530

    Added #7

--- main.c
+++ main.c
@@ -88,6 +88,26 @@ void main()
   }
   case 7:
   {
+printf("Enter Size of Array");
+int a; scanf("%d",&a);
+int arr[a];
+for(int i=0;i<a;i++)
+{scanf("%d",&arr[i]);
+}
+
+int i, j; 
+   for (i = 0; i < a-1; i++)       
+       for (j = 0; j < a-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              {int temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
+              }
+printf("Array in ascending order is:");
+for(i=0;i<a;i++)
+printf("%d ",arr[i]);
+printf("\n");
+
     break;
   }
   case 8:
