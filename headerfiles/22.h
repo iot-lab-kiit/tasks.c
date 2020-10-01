9d4d567b1887151cc506fb4d6a7abac3b6d58a05
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2020-10-01 17:22:03 +0530

    Update 22.h

---
+++ headerfiles/22.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// function to get matrix elements entered by the user
+void getMatrixElements(int matrix[][10], int row, int column) {
+
+   printf("\nEnter elements: \n");
+
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         printf("Enter a%d%d: ", i + 1, j + 1);
+         scanf("%d", &matrix[i][j]);
+      }
+   }
+}
+
+// function to multiply two matrices
+void multiplyMatrices(int first[][10],int second[][10],int result[][10],int r1, int c1, int r2, int c2) 
+{
+
+   // Initializing elements of matrix mult to 0.
+   for (int i = 0; i < r1; ++i) {
+      for (int j = 0; j < c2; ++j) {
+         result[i][j] = 0;
+      }
+   }
+
+   // Multiplying first and second matrices and storing it in result
+   for (int i = 0; i < r1; ++i) {
+      for (int j = 0; j < c2; ++j) {
+         for (int k = 0; k < c1; ++k) {
+            result[i][j] += first[i][k] * second[k][j];
+         }
+      }
+   }
+}
+
+// function to display the matrix
+void display(int result[][10], int row, int column) {
+
+   printf("\nOutput Matrix:\n");
+   for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < column; ++j) {
+         printf("%d  ", result[i][j]);
+         if (j == column - 1)
+            printf("\n");
+      }
+   }
+}
