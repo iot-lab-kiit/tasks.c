5140e369455c18ca17c2955b6e1ef1c3e1e69382
Akshat Rana <65679227+Akshat511716@users.noreply.github.com>
2020-10-01 22:42:59 +0530

    Size of array
    To give output as the total number of elements in an array

---
+++ Size of array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = {1,2,3,4,5,6,7,8,9,10,11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout<<"The length is "<<n;
+	return 0;
+}
