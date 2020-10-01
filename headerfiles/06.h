8c5270a51ee56f4daaf1a43334c5bb552714669c
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-10-01 20:09:20 +0530

    Update 06.h

--- headerfiles/06.h
+++ headerfiles/06.h
@@ -1,9 +1,12 @@
-#include<stdio.h>
-void revString(){
-    char c;
-    scanf("%c", &c);
-    if ( c != '\n'){
-        revString();
-        printf("%c", c);
-    }
+#include <stdio.h>
+void rev(char *g, int begin, int end)
+{
+   char h;
+   if (begin >= end)
+      return;
+
+   h = *(g+begin);
+   *(g+begin) = *(g+end);
+   *(g+end)   = h;
+   rev(g, ++begin, --end);
 }
