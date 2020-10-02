7ff436f3a4357e7eb0defb9dfb06473122dfdd7f
abhinav-193 <67187442+abhinav-193@users.noreply.github.com>
2020-10-02 16:32:18 +0530

    Added header file #3

--- main.c
+++ main.c
@@ -77,7 +77,26 @@ void main()
   }
   case 3:
   {
-    break;
+    int i,j,count;
+   char str[25][25],temp[25];
+   puts("How many strings u are going to enter?: ");
+   scanf("%d",&count);
+
+   puts("Enter Strings one by one: ");
+   for(i=0;i<=count;i++)
+      gets(str[i]);
+   for(i=0;i<=count;i++)
+      for(j=i+1;j<=count;j++){
+         if(strcmp(str[i],str[j])>0){
+            strcpy(temp,str[i]);
+            strcpy(str[i],str[j]);
+            strcpy(str[j],temp);
+         }
+      }
+   printf("Order of Sorted Strings:");
+   for(i=0;i<=count;i++)
+      printf(str[i]);
+
   }
   case 4:
   {
