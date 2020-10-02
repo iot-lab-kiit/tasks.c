8eaf8c2ed698b23c46d6b205f24f41dbac42202a
Abhisek samantaray <abhisekcsce@gmail.com>
2020-10-02 13:09:54 +0530

    updating 31.h Header File

--- headerfiles/31.h
+++ headerfiles/31.h
@@ -1,34 +1,29 @@
-#include <stdio.h>
-void MeanMedian()
-{
-	int i,ar[50],n,temp,j,index,max=0;
-	float sum=0;
-	printf("Enter the no. of elements:");
-	scanf("%d",&n);
-    	printf("Enter the elements:");
-	for(i=0;i<n;i++)
-    	scanf("%d",&ar[i]);
-   	 printf("Array:\n");
-	for(i=0;i<n;i++)
-    	printf("%d ",ar[i]); 
-//Mean  
-	for(i=0;i<n;i++)
-	{   
-    	temp=0;
-    	sum+=ar[i];
- 	}
- 	printf("\nMean = %f\n",(float)sum/n);
-	 //Median
- 	for(i=0;i<n-1;i++)
-    	for(j=0;j<n-i-1;j++)
-        	if(ar[j]>ar[j+1])
-        	{
-        	temp=ar[j];
-        	ar[j]=ar[j+1];
-        	ar[j+1]=temp;
-        	}
- 	if(n%2==0)
-    	{printf("Median = %d & %d\n",ar[(n-1)/2],ar[((n-1)/2)+1]);}
- 	else
-    	{printf("Median = %d\n",ar[((n-1)/2)]);}
-  	 }
+#include <stdio.h> 
+void swap(int *x, int *y) { 
+    int temp = *x; 
+    *x = *y; 
+    *y = temp; 
+} 
+double findMean(int arr[], int n) { 
+    int sum = 0; 
+    for (int i=0;i<n;i++)  
+        sum = sum + arr[i]; 
+      
+    return (double)sum/(double)n; 
+}
+double findMedian(int arr[], int n){ 
+    int i, j, min=0; 
+   
+    for (i=0;i<n-1;i++) { 
+        min= i; 
+        for (j=i+1;j<n;j++) 
+            if (arr[j]<arr[min]) 
+                min=j; 
+  
+        swap(&arr[min], &arr[i]); 
+    } 
+    if (n % 2 != 0) 
+       return (double)arr[n/2]; 
+      
+    return (double)(arr[(n-1)/2] + arr[n/2])/2.0; 
+} 
