39a15868273b3690b938778a7ba4c22b4a5afa5c
Sahil Kumar <io.sahilkr@gmail.com>
2020-10-02 16:22:26 +0530

    removed comments from case 7

--- main.c
+++ main.c
@@ -152,14 +152,12 @@ void main()
   case 11:
   {
     int num1, num2, num3;
-    //taking input from user
     printf("Enter First Number: ");
     scanf("%d",&num1);
     printf("Enter Second Number: ");
     scanf("%d",&num2);
     printf("Enter Third Number: ");
     scanf("%d",&num3);
-    //calling the header function
     largestOfThree(num1, num2, num3);
     break;
   }
