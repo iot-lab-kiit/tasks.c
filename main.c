82fdfd8991cda5666bfff7729aec8e49e62a8ff4
Dipti Yadav <55543923+diptiyadav49@users.noreply.github.com>
2020-10-04 19:51:54 +0530

    changed case 11

--- main.c
+++ main.c
@@ -179,13 +179,41 @@ void main()
   case 11:
   {
     int num1, num2, num3;
+    int *p1, *p2, *p3;
     printf("Enter First Number: ");
     scanf("%d", &num1);
     printf("Enter Second Number: ");
     scanf("%d", &num2);
     printf("Enter Third Number: ");
     scanf("%d", &num3);
-    largestOfThree(num1, num2, num3);
+
+    p1 = &num1;
+    p2 = &num2;
+    p3 = &num3;
+
+    if(*p1 > *p2)
+    {
+	    if(*p1 > *p3)
+	    {
+	     	printf("%d is the largest number", *p1);
+	    }
+	    else
+	    {
+		    printf("%d is the largest number", *p3);
+	    }
+    }
+    else
+    {
+	    if(*p2 > *p3)
+	    {
+	    	printf("%d is the largest number", *p2);
+	    }
+	    else
+	    {
+	     	printf("%d is the largest number", *p3);
+	    }
+    }
+
     break;
   }
   case 12:
