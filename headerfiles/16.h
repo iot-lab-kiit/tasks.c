52bc715da1136bd11971e2646cea9ead1fc6054a
rawatmihir <71319388+rawatmihir@users.noreply.github.com>
2020-10-04 23:22:33 +0530

    Update 16.h
    in the previous code, the machine first calculated factorial(n) and then divided that by factorial(n-r).
    it will be more efficient to just multiply the numbers from (n) to (n-r+1) and return it.

--- headerfiles/16.h
+++ headerfiles/16.h
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int factorial(int n){
-	if(n<=1)
-		return 1;
-	return n*factorial(n-1);
+        if(n<=1)
+                return 1;
+        return n*factorial(n-1);
 }
-int permutation(int n, int r){
-	return factorial(n)/factorial(n-r);
+int permutation(int n, int r){				//factorial(n)/factorial(n-r)
+        if(!r)	return 1;				//if(r==0){ return 1;}
+							//else
+	return n*permutation(n-1, r-1);			//this is equivalent to return(n * (n-1) * (n-2) * ..... * (n-r+1))
 }
-
-
