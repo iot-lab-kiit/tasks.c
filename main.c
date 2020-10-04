70772c2932e1f0520c99264eb3e4621d1b0e4585
Dipti Yadav <55543923+diptiyadav49@users.noreply.github.com>
2020-10-04 22:04:13 +0530

    updated case 10

--- main.c
+++ main.c
@@ -176,7 +176,6 @@ void main()
   {
 	  // number of elements in an array
 	  int arr[]= {10,20,30,40,50};
-	  int length;
 	  
 	  int length = sizeof(arr) / sizeof(arr[0]); 
 	  printf("Number of elements present in the array: %d", length); 
