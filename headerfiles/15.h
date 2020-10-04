1f74c32253e6eb88ab3aa227fe2d847d6be12fa9
SwethaVipparla <swetha.vipparla@students.iiit.ac.in>
2020-10-04 20:09:21 +0400

    Added solution for case 15

--- headerfiles/15.h
+++ headerfiles/15.h
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+
+int actionsonpointer()
+{
+    char ch;
+
+    // pointer
+    char *chP;
+
+    // Initialize pointer variable with the address of ch variable
+    chP = &ch;
+
+    // Assigning the letter 'S' to ch
+    ch = 'S';
+
+    // Access the value and address using ch
+    printf("Value of ch is %c\n", ch);
+    printf("Address of ch is %p\n", &ch);
+
+    // Access the value and address using pointer variable chP
+    printf("Value of ch pointer is: %c\n", *chP);
+    printf("Address of ch pointer is %p\n", chP);
+
+   return 0;
+}
