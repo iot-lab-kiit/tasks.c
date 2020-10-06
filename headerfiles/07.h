52e42644c60210f99d35add8323a0f130fe39855
CHANDAN KUMAR SINGH <53275158+chandan5569@users.noreply.github.com>
2020-10-06 11:14:46 +0530

    Update 07.h

--- headerfiles/07.h
+++ headerfiles/07.h
@@ -1,21 +1,24 @@
 #include<stdio.h>
 void Sort_ele(int n, int a[])
 {
-    int x;
-    for(int i=0;i<=n;i++)
-        {
-            for(int j=i+1;j<=n;j++)
-            {
-
-                if(a[i]>a[j])
-                {
-                    x=a[i];
-                    a[i]=a[j];
-                    a[j]=x;
-                }
-            }
-        }
-    printf("The numbers arranged in ascending order is given below :\n");
+  for (i = 0; i < (n - 1); i++) 
+  {
+    pos = i;               /* Finding the minimum element n-1 times and then compare it with the first element of the array. if it is less than first element then swap the current number with the first number of the array. */
+      
+    for (j = i + 1; j < n; j++)        /*Selection sort algorithm */
+    {
+      if (a[pos] > a[j])
+        pos = j;
+    }
+    if (pos != i)
+    {
+      t = a[i];
+      a[i] = a[pos];
+      a[pos] = t;
+    }
+  }
+    
+printf("The numbers arranged in ascending order is given below :\n");
     for(int i=0;i<=n;i++)
         printf("%d\n",a[i]);
 }
