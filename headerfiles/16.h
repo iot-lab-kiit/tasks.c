08ac65a3063de7f0e01254a052e9828b4b6cca43
CHANDAN KUMAR SINGH <53275158+chandan5569@users.noreply.github.com>
2020-10-06 17:03:05 +0530

    Update 16.h

--- headerfiles/16.h
+++ headerfiles/16.h
@@ -1,12 +1,30 @@
 #include<stdio.h>
 
-int factorial(int n){
-        if(n<=1)
-                return 1;
-        return n*factorial(n-1);
+/* Finding the factorial first. */
+
+int fact(int n)
+{
+
+int f=1;
+
+while(n!=0)
+{
+
+f=f*n;
+
+n--;
 }
-int permutation(int n, int r){				//factorial(n)/factorial(n-r)
-        if(!r)	return 1;				//if(r==0){ return 1;}
-							//else
-	return n*permutation(n-1, r-1);			//this is equivalent to return(n * (n-1) * (n-2) * ..... * (n-r+1))
+
+	/*Now calculating the permutation for the given nPr. */
+	
+	/*Formulla for nPr is n!/(n-r)! */
+	
+	
+int permutation(int n,int r)
+{
+
+return (fact(n)/fact(n-r));
+
 }
+
+
