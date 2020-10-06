621eb409b5779da1a248db17217f2d5944a0b76f
CHANDAN KUMAR SINGH <53275158+chandan5569@users.noreply.github.com>
2020-10-06 19:06:12 +0530

    Update 24.h

--- headerfiles/24.h
+++ headerfiles/24.h
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int Largest(int* a, int n)
-{
+{                                     /* here we are assigning the value starting address of the array in the pointer type variable a. for example a=arr[0] */
     int i;
     for (i=1; i<n; i++)
     {
-        if(*a < *(a+i))
+        if(*a < *(a+i))              /*comparing the each value with the intial value of the array. if less than the intial value then we assign it  to the starting of the array. */
            *a = *(a+i);
     }
     printf("The largest element is %d ", *a);
