0e7183586623acf05aa63998db2af148ceb3a142
AnkitaS28 <ankita102rocks@gmail.com>
2020-10-09 14:55:44 +0530

    Program to sort elements in Lexicographical order

--- main.c
+++ main.c
@@ -278,15 +278,7 @@ void main()
     printf("Enter a value for the divisor: ");
     scanf("%d", &divisor);
 
-    //computing quotient
-    quotient = dividend/divisor;
-
-    //computing remainder
-    remainder = dividend % divisor;
-
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-
+    quotientandremainder(dividend, divisor);
     break;
   }
   case 20:
@@ -392,20 +384,34 @@ void main()
     break;
   }
   case 25:
-  {
-    char stringMatrix[100][100];
-    //Find the number of words
-    int numberOfWords;
-    printf("\nEnter the number of words: ");
-    scanf("%d", &numberOfWords);
-    //Input the words
-    getWords(stringMatrix, numberOfWords);
-    //Sort the words in lexicographical order
-    sortWords(stringMatrix, numberOfWords);
-    //Print the words
-    printWords(stringMatrix, numberOfWords);
-    break;
-  }
+   {
+   char str[5][50], temp[50];
+   printf("Enter 5 words: ");
+
+   // Getting strings input
+   for (int i = 0; i < 5; ++i) {
+      fgets(str[i], sizeof(str[i]), stdin);
+   }
+
+   // storing strings in the lexicographical order
+   for (int i = 0; i < 5; ++i) {
+      for (int j = i + 1; j < 5; ++j) {
+
+         // swapping strings if they are not in the lexicographical order
+         if (strcmp(str[i], str[j]) > 0) {
+            strcpy(temp, str[i]);
+            strcpy(str[i], str[j]);
+            strcpy(str[j], temp);
+         }
+      }
+   }
+
+   printf("\nIn the lexicographical order: \n");
+   for (int i = 0; i < 5; ++i) {
+      fputs(str[i], stdout);
+   }
+   return 0;
+}
   case 26:
   {
     struct time start, stop, diff;
