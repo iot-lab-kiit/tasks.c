b3f3b38942ee1fec70710260c2bc963585cc6f4e
hariom09090 <hariom24568@gmail.com>
2020-10-11 22:14:54 +0530

    using sizeof to calculate size of array

--- headerfiles/10.h
+++ headerfiles/10.h
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-
-void count(int n)
+int main()
 {
-    printf("The number of elements in the array is: %d\n", n);
+    int size, i;
+    printf("Enter size:");        // i have take input , but doesn't use it in my logic
+    scanf("%d", &size);
+    int ar[size];
+    printf("Enter the element \n");
+    for (i = 0; i < size; i++)
+        scanf("%d", &ar[i]);
+
+    int calculateSize;
+    calculateSize = sizeof(ar) / sizeof(ar[0]);        // it means dividing the size of  whole array by size of single array element
+    printf("The size of array is : %d", calculateSize);
+    return 0;
 }
