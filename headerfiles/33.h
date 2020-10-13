519ab2669e60a07609bd2a4657c5d0222a886282
shivamyadav42001 <1906505@kiit.ac.in>
2020-10-13 21:20:36 +0530

    prime no through recursion

--- headerfiles/33.h
+++ headerfiles/33.h
@@ -1,32 +1,5 @@
 //prime no through recursion
-
 #include<stdio.h>
-
-int checkForPrime(int);
-int i;
-
-int main()
-{
-
-    int n1,primeNo;
-
-	printf("\n\n Recursion : Check a number is prime number or not :\n");
-	printf("--------------------------------------------------------\n");
-	
-    printf(" Input any positive number : ");
-    scanf("%d",&n1);
-    
-    i = n1/2;
-
-    primeNo = checkForPrime(n1);//call the function checkForPrime
-
-   if(primeNo==1)
-        printf(" The number %d is a prime number. \n\n",n1);
-   else
-      printf(" The number %d is not a prime number. \nn",n1);
-   return 0;
-}
-
 int checkForPrime(int n1) // recursive function
 {
     if(i==1)
