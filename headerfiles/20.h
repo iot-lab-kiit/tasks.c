c7e0bd294e1e344e3a96c5213f33d7f67f9ba2b5
wrathofgawd <72682190+wrathofgawd@users.noreply.github.com>
2020-10-18 21:24:18 +0530

    Added comments and made the function to return
    Added comments to make the program understandable more
    and made the function the return average to the main function

--- headerfiles/20.h
+++ headerfiles/20.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-void avg(int n1,int n2)
+#include <stdio.h> //basic required header file.
+float avg(int n1,int n2) //defining function avg with two parameters i.e., the numbers whose average is to be found out.
 {
-    float avg;
-    avg= (float)(n1+n2)/2;
-    printf("Average of %d and %d is: %.2f",n1,n2,avg);
+   float average; //declaring variable average as float.
+   avg= (float)(n1+n2)/2; // dividing by 2 and explicit conversion of int is done into float.
+   return average; //returning average of the numbers.
 }
