1d2ef91f9b7931dc2e812311a2db127b52771275
“kousani0416” <56110208+kousani0416@users.noreply.github.com>
2020-10-22 01:08:36 +0530

    handled some corner cases

--- headerfiles/33.h
+++ headerfiles/33.h
@@ -13,6 +13,8 @@ void check_prime(int n)
             break;
         }
     }
+
+    // if number is not prime if it has factors other than 1 and itself or if the number is less than 2
     if (flag == 1 || n < 2)
         printf("\n %d is not prime \n",n);
     else
